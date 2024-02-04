f35004fcff85397c9622c48a93b8a400a202b329
Naurangi_lal <naurangilal15072002@gmail.com>
2024-02-04 13:42:07 +0530

    publish commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
@@ -13,19 +12,6 @@ export default defineConfig({
     },
   },
   build: {
-    manifest: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id
-              .toString()
-              .split('node_modules/')[1]
-              .split('/')[0]
-              .toString();
-          }
-        },
-      },
-    },
+    chunkSizeWarningLimit: 1500,
   },
 });
