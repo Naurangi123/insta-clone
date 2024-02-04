aacb219c657b70fa9e20a0937055f3257261bec5
Naurangi_lal <naurangilal15072002@gmail.com>
2024-02-04 12:28:06 +0530

    PUBLISH COMMIT

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAx1ttn_4yV20_1DoDjJGkORoCc8VjCK-k',
-  authDomain: 'instagram-clone-7c46f.firebaseapp.com',
-  projectId: 'instagram-clone-7c46f',
-  storageBucket: 'instagram-clone-7c46f.appspot.com',
-  messagingSenderId: '341250653587',
-  appId: '1:341250653587:web:c7907bd11e615d177b3c66',
-  measurementId: 'G-Q54NHTK33K',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
